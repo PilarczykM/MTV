e4853915bf60aa6f22f66969acea6ff7fddbad3d
Marcin Pilarczyk <marcinpilarczyk@me.com>
2025-04-12 16:23:55 +0200

    Include REST routing

--- backend/main.py
+++ backend/main.py
@@ -4,13 +4,16 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 
+from backend.api.routes.tests import router as tests_router
+from backend.repository.test_csv_repository import TestCSVRepository
 from backend.ws.socket import simulate_shared_tests_stream, websocket_endpoint
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):  # noqa: ARG001, app required.
+async def lifespan(app: FastAPI):
     """Run lifespan."""
     simulation_task = asyncio.create_task(simulate_shared_tests_stream())
+    app.state.test_repository = TestCSVRepository()
     yield
     simulation_task.cancel()
     try:
@@ -28,6 +31,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(tests_router)
 
 
 @app.websocket("/ws/execution")
