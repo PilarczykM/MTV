95ba8454ba7ae8df4ad90b3b03f338ab00fc6600
Marcin Pilarczyk <marcinpilarczyk@me.com>
2025-04-13 22:25:20 +0200

    Rename file into shared

--- mtv_dashboard/main.py
+++ mtv_dashboard/main.py
@@ -1,7 +1,7 @@
 from dash import Dash
 
 from mtv_dashboard.callback import websocket  # noqa: F401
-from mtv_dashboard.callback.base import register_clientside_callbacks
+from mtv_dashboard.callback.shared import register_clientside_callbacks
 
 app = Dash(__name__, suppress_callback_exceptions=True, title="MTV", use_pages=True)
 server = app.server
